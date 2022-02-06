e495d7a8e5ead8f4a97820cacc7f1b6f07a0c186
seb1907 <92942138+seb1907@users.noreply.github.com>
2022-02-06 17:50:12 +0400

    Add files via upload

---
+++ Hackathon2/static/script.js
@@ -0,0 +1,35 @@
+
+
+function recievedProducts() {
+    console.log('Recieved Products Selected')
+}
+function withdrawnProducts() {
+    console.log('Withdrawn Products Selected')
+}
+function stockUpdate() {
+    console.log('Update Stock Selected')
+}
+
+function suppliers() {
+    console.log('suppliers list')
+}
+function usersList() {
+    console.log('User List')
+}
+function todoList() {
+    console.log('To Do List')
+}
+
+let recieved = document.getElementById('recieved')
+let withdrawn = document.getElementById('withdrawn')
+let update = document.getElementById('update')
+let supliers = document.getElementById('supp_list')
+let userlist = document.getElementById('user_list')
+let todolist = document.getElementById('todo_list')
+
+recieved.onclick = recievedProducts
+withdrawn.onclick = withdrawnProducts
+update.onclick = stockUpdate
+supliers.onclick= suppliers
+userlist.onclick = usersList
+todolist.onclick = todoList
