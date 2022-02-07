217a15e99b4679bc7c14e0ebad445a79a7a8a89b
seb1907 <92942138+seb1907@users.noreply.github.com>
2022-02-07 16:18:31 +0400

    Add files via upload

--- Hackathon2/main.py
+++ Hackathon2/main.py
@@ -8,9 +8,29 @@ app = Flask(__name__)
 def index():
    return render_template('main.html')
 
+@app.route("/result", methods=['POST'])
+def result():
+    return render_template('recieved.html')
 
+@app.route("/stock")
+def stock():
+    return render_template('stock.html')
 
+@app.route("/suppliers")
+def suppliers():
+    return render_template('suppliers.html')
 
+@app.route("/Todo")
+def todo():
+    return render_template('todo.html')
+
+@app.route("/users")
+def users():
+    return render_template('users.html')
+
+@app.route("/withdrawn")
+def withdrawn():
+    return render_template('withdrawn.html')
 
 
 if __name__ == "__main__":
