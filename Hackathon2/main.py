8ce6385653475049463b4875d102339983d7f811
seb1907 <92942138+seb1907@users.noreply.github.com>
2022-02-13 03:24:12 +0400

    Add files via upload

--- Hackathon2/main.py
+++ Hackathon2/main.py
@@ -8,9 +8,36 @@ app = Flask(__name__)
 def index():
    return render_template('main.html')
 
+@app.route("/logscreen")
+def log_screen():
+    return render_template('start_screen.html')
 
+@app.route("/result")
+def result():
+      return render_template('received.html')
 
+@app.route("/stock")
+def stock():
+    return render_template('stock.html')
 
+@app.route("/suppliers")
+def suppliers():
+    return render_template('suppliers.html')
+
+@app.route("/Todo")
+def todo():
+    return render_template('todo.html')
+
+@app.route("/users")
+def users():
+    return render_template('users.html')
+
+@app.route("/withdrawn")
+def withdrawn():
+    return render_template('withdrawn.html')
+@app.route("/login")
+def log():
+    return render_template('login_page.html')
 
 
 if __name__ == "__main__":
---
+++ Hackathon2/static/login.js
@@ -0,0 +1,10 @@
+
+let login = document.getElementById('logIn')
+
+ function log_in() {
+     console.log('Login selected')
+}
+
+login.onclick = log_in
+
+// export {log_in};
